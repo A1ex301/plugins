48f57f8c919ab23ff51fe3fb33302a97e014124e
A1ex301 <makhynia2539@gmail.com>
2025-04-08 15:49:57 +0300

    Update ratingimdb.js

--- ratingimdb.js
+++ ratingimdb.js
@@ -17,23 +17,7 @@
     }
 
     function renderIMDb(rating) {
-        return `
-        <div class="imdb-rating" style="
-            display: inline-flex;
-            align-items: center;
-            gap: 6px;
-            margin-top: 4px;
-            font-size: 14px;
-            font-weight: bold;
-            background: #f5c518;
-            color: black;
-            padding: 2px 6px;
-            border-radius: 4px;
-            width: fit-content;
-        ">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg" alt="IMDb" style="height: 16px;">
-            <span>${rating}</span>
-        </div>`;
+        return `<div class="imdb-rating">IMDb: ${rating}</div>`;
     }
 
     function appendRating(element, rating) {
